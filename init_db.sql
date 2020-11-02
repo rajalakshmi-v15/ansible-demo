2f1885b866007bfd5d0682f76218eb26c2cbc4ae
rajalakshmi-v15 <rajalakshmi.v15@iiits.in>
2020-11-02 16:13:02 +0000

    Changed mysql password information

--- init_db.sql
+++ init_db.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE IF NOT EXISTS employee_db;
-GRANT ALL ON *.* to db_user@localhost IDENTIFIED BY 'password';
+GRANT ALL ON *.* to db_user@localhost IDENTIFIED BY 'Passw0rd';
 USE employee_db;
 CREATE TABLE employees (name VARCHAR(20));
 INSERT INTO employees VALUES ('JOHN');
