c5c7c4709f266a950709a033782e1e04db364b79
rajalakshmi-v15 <rajalakshmi.v15@iiits.in>
2020-11-02 14:41:25 +0000

    SQL statements

---
+++ init_db.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE employee_db;
+GRANT ALL ON *.* to db_user@localhost IDENTIFIED BY 'password';
+USE employee_db;
+CREATE TABLE employees (name VARCHAR(20));
+INSERT INTO employees VALUES ('JOHN');
